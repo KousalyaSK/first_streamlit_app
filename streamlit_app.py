2cf7915cd48181f7f2546b5ce3321ce2450e1f22
KousalyaSK <141231055+KousalyaSK@users.noreply.github.com>
2023-08-07 17:36:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,5 +38,5 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 #creating connector internal stage
-import snowflake.connector
+
 
