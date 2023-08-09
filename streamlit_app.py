5527accc8ad817bad9ef5c4b67a5b9491e0e291a
KousalyaSK <141231055+KousalyaSK@users.noreply.github.com>
2023-08-09 13:21:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,6 +78,10 @@ if streamlit.button('Add a fruit to the list'):
     my_cnx.close()  
     streamlit.text(back_from_function)
 
+import pandas
+my_file = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/smew/ISO_Countries_UTF8_pipe.csv")
+streamlit.dataframe(my_file)
+
 
 
 
